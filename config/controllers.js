2c3b12ba472f7585281e89b110e043689ae7e47e
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-11-24 10:27:23 -0800

    add pagination, links do not disable properly yet

--- config/controllers.js
+++ config/controllers.js
@@ -5,8 +5,21 @@ angular
   .controller('homeCtrl', function($scope, $rootScope, planetsService){
     $scope.title = "Planets"
     $scope.residents = $rootScope.residents
-    planetsService.getPlanets();
-    $scope.planets = $rootScope.planets;
+    $scope.page = 1;
+
+    $scope.getNextPage = function(){
+      $scope.page++;
+      planetsService.getPlanets($scope.page);
+      $scope.planets = $rootScope.planets[$scope.page];
+    }
+
+    $scope.getPreviousPage = function(){
+      $scope.page--;
+      $scope.planets = $rootScope.planets[$scope.page];
+    }
+
+    planetsService.getPlanets($scope.page);
+    $scope.planets = $rootScope.planets[$scope.page];
   })
   .controller('residentCtrl', function($scope, resident){
     $scope.title = "Resident";
--- config/services.js
+++ config/services.js
@@ -4,17 +4,23 @@ angular
   .module('myApp')
 
   .service('planetsService', function($http, $rootScope){
-    $rootScope.planets = [];
-    this.getPlanets = function(){
-      $http.get('http://swapi.co/api/planets/', {cache: true}).then(function(res){
-        $rootScope.planets = res.data.results.map(function(planet){
+    $rootScope.planets = {};
+    this.getPlanets = function(page){
+      $rootScope.planets[page] = $rootScope.planets[page] ? $rootScope.planets[page] : [];
+      if ($rootScope.planets[page].length){
+        return $rootScope.planets[page];
+      }
+      $http.get('http://swapi.co/api/planets/?page=' + page, {cache: true}).then(function(res){
+        console.log(res.data);
+        console.log('here', $rootScope.planets[page]);
+        $rootScope.planets[page] = res.data.results.map(function(planet){
           planet.residents = planet.residents.map(function(resident){
             var resident = { url : resident};
             resident.id = resident.url.match(/\d+/)[0];
-            // console.log(resident);
             return resident;
           });
-          $rootScope.planets.push(planet);
+          $rootScope.planets[page].push(planet);
+          console.log('there', $rootScope.planets)
           return planet;
         });
       }).catch(function(err){
